34d2e48c3279511a86ef4332804acfa1315384a7
JanhaviDalvi <janhavidalvi3008@gmail.com>
2023-03-14 21:19:58 +0530

    about to push on github

--- public/kanban.js
+++ public/kanban.js
@@ -1,97 +1,9 @@
 var cardName, cardDescription, cardTag, cardPriority, cardDueDate, currentDate, status;
 console.log(userId);
 
-// get the submit button
-// var submitBtn = document.getElementById("submitCard");
-
-// attach an event listener to the submit button
 window.onload = function () {
     console.log("window loaded");
-    // // get the value of the cardName input
-    // var cardName = document.getElementById("cardName").value;
-    // // console.log(cardName);
-
-    // var cardDescription = document.getElementById("cardDescription").value;
-    // // console.log(cardDescription);
-
-    // var cardTag = document.getElementById("cardTag").value;
-    // // console.log(cardTag);
-
-    // var cardPriority = document.getElementById("cardPriority").value;
-    // // console.log(cardPriority);
-
-    // // var cardCreationDate = document.getElementById("cardCreationDate").value;
-    // // console.log(cardCreationDate);
-
-    // var cardDueDate = document.getElementById("cardDueDate").value;
-    // // console.log(cardDueDate);
-
-    // const currentDate = new Date().toDateString();
-
-    // var status = 'todo';
-
-    // // create a new card element
-    // var newCard = document.createElement("div");
-    // newCard.classList.add("card");
-
-    // // create the card header
-    // var cardHeader = document.createElement("div");
-    // cardHeader.classList.add("card-header");
-    // cardHeader.innerHTML = cardName;
-
-    // // create the card body
-    // var cardBody = document.createElement("div");
-    // cardBody.classList.add("card-body");
-
-    // // create the description
-    // var description = document.createElement("div");
-    // description.classList.add("card_discription");
-    // description.innerHTML = "Description: " + cardDescription;
-
-
-    // // create the tag
-    // var tag = document.createElement("div");
-    // tag.classList.add("card_tag");
-    // tag.innerHTML = "Tag: " + cardTag;
-
-    // // create the priority
-    // var priority = document.createElement("div");
-    // priority.classList.add("card_priority");
-    // let Prioritytext;
-    // if(cardPriority == 1 ){
-    //     Prioritytext = "Low";
-    // }
-    // else if (cardPriority == 2) {
-    //     Prioritytext = "Medium";
-    // }
-    // else {
-    //     Prioritytext = "High";
-    // }
-    // priority.innerHTML = "Priority: " + Prioritytext;
-
-    // // create the due date
-    // var dueDate = document.createElement("div");
-    // dueDate.classList.add("card_duedate");
-    // dueDate.innerHTML = "Due Date: " + cardDueDate;
-
-    // // append elements to the card body
-    // cardBody.appendChild(description);
-    // cardBody.appendChild(tag);
-    // cardBody.appendChild(priority);
-    // cardBody.appendChild(dueDate);
-
-    // // append elements to the new card
-    // newCard.appendChild(cardHeader);
-    // newCard.appendChild(cardBody);
-
-    // append the new card to the to-do column
-    var todo_column = document.getElementById("todo_column");
-    // newCard.setAttribute("draggable", "true")
-    // todo_column.appendChild(newCard);
-
-    // $('#exampleModal').modal('toggle');
-    // document.getElementById("kanban_form").reset();
-
+    
     var all_cards = document.querySelectorAll(".card");
     console.log("sdjfjsdkfhj", all_cards);
     all_cards.forEach(each_card => {
@@ -108,13 +20,6 @@ window.onload = function () {
         });
     });
 };
-
-// var closecard = document.getElementById("closecard");
-// // attach an event listener to the submit button
-// closecard.addEventListener("click", function () {
-//     $('#exampleModal').modal('toggle');
-//     document.getElementById("kanban_form").reset();
-// });
 
 // dragging
 const column1 = document.getElementById("todo_column");
@@ -200,6 +105,7 @@ sort.addEventListener("change", function () {
     // for(let i = 0; i < columns.length; i++){
     //     sort_cards(columns[i], sortBy);
     // }
+    console.log(sortBy);
     sort_cards(column1, sortBy);
 });
 
